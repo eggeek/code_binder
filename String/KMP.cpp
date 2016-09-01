292218d55d6bdff92bd9bee662d9ccae181ec3c0
eggeek <zsz921130@gmail.com>
2016-09-01 19:24:42 +1000

    format KMP

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,41 +1,57 @@
-#include<stdio.h>
-#include<string.h>
-#define maxn 1000010
-char p[maxn],txt[maxn];
-int next[maxn];
-int lenp,lent;
-void getNext()
-{
-    int i,j;
-    next[0]=-1,j=-1;
-    for(i=0;i<lenp;){
-        while(j!=-1 && p[i]!=p[j])j=next[j];
-        i++;j++;
-        next[i]=j;
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+typedef vector<int> vi;
+
+// test on: uva455
+class KMP {
+public:
+  string pattern;
+  vi next;
+  void init(string str) {
+    pattern = str;
+    next.resize((int)str.size()+1);
+  }
+
+  void getNext() {
+    int lenp = (int)pattern.size();
+    next[0] = -1;
+    for (int i=0, j=-1; i<lenp;) {
+      while (j != -1 && pattern[i] != pattern[j]) {
+        j = next[j];
+      }
+      i++; j++;
+      next[i] = j;
     }
-}
-void kmp()
-{
-    int i,j;
-    int cnt=0;
-    for(i=0,j=0;i<lent;i++){
-        while(j!=-1 && txt[i]!=p[j])j=next[j];
-        j++;
-        if(j==lenp)cnt++;
+  }
+
+  int match(string text) { // return the number of pattern in text
+    int i, j;
+    int cnt = 0, lent = (int)text.size(), lenp = (int)pattern.size();
+    for (i=0, j=0; i<lent; i++) {
+      while (j != -1 && text[i] != pattern[j]) j = next[j];
+      j++;
+      if (j == lenp) cnt++;
     }
-    printf("%d\n",cnt);
-}
-int main()
-{
+    return cnt;
+  }
+} sol;
+
+int main() {
+    bool flag = false;
     int cas;
-//    freopen("test.txt","r",stdin);
-    scanf("%d",&cas);
-    while(cas--){
-        scanf("%s%s",p,txt);
-        lenp=strlen(p);
-        lent=strlen(txt);
-        getNext();
-        kmp();
+    cin >> cas;
+    while(cas--) {
+        if (!flag) flag = true;
+        else cout << endl;
+        string p;
+        cin >> p;
+        sol.init(p);
+        sol.getNext();
+        int len = (int)p.size();
+        int ans = len % (len - sol.next[len])? len: len - sol.next[len];
+        cout << ans << endl;
     }
     return 0;
 }
